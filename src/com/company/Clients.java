79298e88351a36c77dc3dd1866cc2f4fe3230905
rodrigoofig <rodres2002@gmail.com>
2021-12-08 17:40:11 +0000

    the last java doc

--- src/com/company/Clients.java
+++ src/com/company/Clients.java
@@ -3,18 +3,47 @@ package com.company;
 import java.io.Serializable;
 import java.util.ArrayList;
 
-
+/**
+ * class for the clients
+ */
 abstract class Clients implements Serializable {
+    /**
+     * name of the client
+     */
     private String name;
+    /**
+     * household of the client
+     */
     private String household;
+    /**
+     * email of the client
+     */
     private String email;
+    /**
+     * phone number of the client
+     */
     private String phoneNum;
+    /**
+     * birthday Date of the client
+     */
     private Date birhtday;
 
-
+    /**
+     * empty constructor of the class Clients
+     */
     public Clients() {
     }
 
+    /**
+     *
+     * inicializes the variables of a Clients object
+     *
+     * @param name name of the client
+     * @param household household of the client
+     * @param email email of the client
+     * @param phoneNum phone number of the client
+     * @param birhtday birthday Date of the client
+     */
     public Clients(String name, String household, String email, String phoneNum, Date birhtday) {
         this.name = name;
         this.household = household;
@@ -25,47 +54,90 @@ abstract class Clients implements Serializable {
 
     }
 
+    /**
+     *
+     * @return name of the client
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     *
+     * @param name name of the client
+     */
     public void setName(String name) {
         this.name = name;
     }
 
+    /**
+     *
+     * @return household of the client
+     */
     public String getHousehold() {
         return household;
     }
 
+    /**
+     *
+     * @param household household of the client
+     */
     public void setHousehold(String household) {
         this.household = household;
     }
 
+    /**
+     *
+     * @return email of the client
+     */
     public String getEmail() {
         return email;
     }
 
+    /**
+     *
+     * @param email email of the client
+     */
     public void setEmail(String email) {
         this.email = email;
     }
 
+    /**
+     *
+     * @return phone number of the client
+     */
     public String getPhoneNum() {
         return phoneNum;
     }
 
+    /**
+     *
+     * @param phoneNum phone number of the client
+     */
     public void setPhoneNum(String phoneNum) {
         this.phoneNum = phoneNum;
     }
 
+    /**
+     *
+     * @return birthday Date of the client
+     */
     public Date getBirhtday() {
         return birhtday;
     }
 
+    /**
+     *
+     * @param birhtday birthday Date of the client
+     */
     public void setBirhtday(Date birhtday) {
         this.birhtday = birhtday;
     }
 
-
+    /**
+     *
+     * @return String with the information of the client
+     */
     @Override
     public String toString() {
         return "Clientes{" +
@@ -77,6 +149,13 @@ abstract class Clients implements Serializable {
                 ", valTrans=" +
                 '}';
     }
+    /**
+     *This is a abstract method that calculates the tranport value of a purchase
+     *
+     * @param pValue price paid by the client
+     * @param list Products bought by the user
+     * @return price of the transport
+     */
 
-    abstract double transportValue(double valorPago, ArrayList<Products> list);
+    abstract double transportValue(double pValue, ArrayList<Products> list);
 }
--- src/com/company/Date.java
+++ src/com/company/Date.java
@@ -2,13 +2,37 @@ package com.company;
 
 import java.io.Serializable;
 
+/**
+ * Class for the Dates
+ */
 public class Date implements Serializable {
+    /**
+     * day of the date
+     */
     private int day;
+    /**
+     * month of the date
+     */
     private int month;
+    /**
+     * year of the date
+     */
     private int year;
 
+    /**
+     * empty constructor of the Date class
+     */
     public Date() {
     }
+
+    /**
+     *
+     * inicializes the variables of the Date object
+     *
+     * @param day day of the date
+     * @param month month of the date
+     * @param year year of the date
+     */
 
     public Date(int day, int month, int year) {
         this.day = day;
@@ -16,10 +40,18 @@ public class Date implements Serializable {
         this.year = year;
     }
 
+    /**
+     *
+     * @return day of the date
+     */
     public int getDay() {
         return day;
     }
 
+    /**
+     *
+     * @param day day of the date
+     */
     public void setDay(int day) {
 
         if (day < 0 || day > 31) {
@@ -28,10 +60,18 @@ public class Date implements Serializable {
         this.day = day;
     }
 
+    /**
+     *
+     * @return month of the date
+     */
     public int getMonth() {
         return month;
     }
 
+    /**
+     *
+     * @param month month of the date
+     */
     public void setMonth(int month) {
         if (month < 0 || month > 12) {
             throw new IllegalArgumentException("mes not between 0 and 12");
@@ -39,10 +79,18 @@ public class Date implements Serializable {
         this.month = month;
     }
 
+    /**
+     *
+     * @return year of the date
+     */
     public int getYear() {
         return year;
     }
 
+    /**
+     *
+     * @param year year of the date
+     */
     public void setYear(int year) {
         if (year < 0) {
             throw new IllegalArgumentException("mes not bigger than 0");
@@ -50,17 +98,34 @@ public class Date implements Serializable {
         this.year = year;
     }
 
+    /**
+     *
+     *@return String with the Date
+     */
     @Override
     public String toString() {
         return day + "/" + month + "/" + year;
     }
 
-
+    /**
+     *
+     * compares two dates and returns if the date is equal or not
+     *
+     * @param d Date
+     * @return true or false if the date is equal or not
+     */
     public boolean equals(Date d) {
         if (this == d) return true;
         return day == d.day && month == d.month && year == d.year;
     }
 
+    /**
+     *
+     * compares two dates and returns if the date is bigger or not
+     *
+     * @param d Date
+     * @return returns true or false if the date is bigger or not
+     */
     public boolean dataMaior(Date d) {
         if (month == d.month && year == d.year) {
             return day > d.day;
@@ -74,6 +139,13 @@ public class Date implements Serializable {
         return false;
     }
 
+    /**
+     *
+     * gets a date in form of a string and returns a Date object
+     *
+     * @param date Date String
+     * @return Object Date
+     */
     public Date dateStringToInt(String date) {
         String[] dmy = date.split("/");
         try {
